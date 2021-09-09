261a9e2e80d60e544afdfd237ea517864ee8bb91
Alex Zhou <alex.lawsonzhou@hotmail.com>
2021-09-08 23:58:10 -0400

    more date pipeline code

--- road_signs_network.py
+++ road_signs_network.py
@@ -19,7 +19,7 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from torchvision import models
 
-images_path = Path('./road_signs/images')
+images_path = Path('D:/projects_python/road_signs_network/road_signs/images')
 anno_path = Path('./road_signs/annotations')
 
 
@@ -110,9 +110,90 @@ df_train['new_bb'] = new_bbs
 
 im = cv2.imread(str(df_train.values[58][0]))
 bb = create_bb_array(df_train.values[58])
-print(im.shape)
 
 Y = create_mask(bb, im)
 mask_to_bb(Y)
 
 plt.imshow(im)
+plt.show()
+
+plt.imshow(Y, cmap='gray')
+plt.show()
+
+
+def crop(im, r, c, target_r, target_c):
+    return im[r:r + target_r, c:c + target_c]
+
+
+def random_crop(x, r_pix=8):
+    """returns a random crop"""
+    r, c, *_ = x.shape
+    c_pix = round(r_pix * c / r)
+    rand_r = random.uniform(0, 1)
+    rand_c = random.uniform(0, 1)
+    start_r = np.floor(2 * rand_r * r_pix).astype(int)
+    start_c = np.floor(2 * rand_c * c_pix).astype(int)
+    return crop(x, start_r, start_c, r - 2 * r_pix, c - 2 * c_pix)
+
+
+def center_crop(x, r_pix=8):
+    r, c, *_ = x.shape
+    c_pix = round(r_pix * c / r)
+    return crop(x, r_pix, c_pix, r - 2 * r_pix, c - 2 * c_pix)
+
+
+def rotate_cv(im, deg, y=False, mode=cv2.BORDER_REFLECT, interpolation=cv2.INTER_AREA):
+    """rotates an image by deg degrees"""
+    r, c, *_ = im.shape
+    M = cv2.getRotationMatrix2D((c / 2, r / 2), deg, 1)
+    if y:
+        return cv2.warpAffine(im, M, (c, r), borderMode=cv2.BORDER_CONSTANT)
+    return cv2.warpAffine(im, M, (c, r), borderMode=mode, flags=cv2.WARP_FILL_OUTLIERS + interpolation)
+
+
+def random_cropXY(x, Y, r_pix=8):
+    """returns a random crop"""
+    r, c, *_ = x.shape
+    c_pix = round(r_pix * c / r)
+    rand_r = random.uniform(0, 1)
+    rand_c = random.uniform(0, 1)
+    start_r = np.floor(2 * rand_r * r_pix).astype(int)
+    start_c = np.floor(2 * rand_c * c_pix).astype(int)
+    xx = crop(x, start_r, start_c, r - 2 * r_pix, c - 2 * c_pix)
+    YY = crop(Y, start_r, start_c, r - 2 * r_pix, c - 2 * c_pix)
+    return xx, YY
+
+
+def transformsXY(path, bb, transforms):
+    x = cv2.imread(str(path)).astype(np.float32)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB) / 255
+    Y = create_mask(bb, x)
+    if transforms:
+        rdeg = (np.random.random() - .50) * 20
+        x = rotate_cv(x, rdeg)
+        Y = rotate_cv(Y, rdeg, y=True)
+        if np.random.random() > 0.5:
+            x = np.fliplr(x).copy()
+            Y = np.fliplr(Y).copy()
+        x, Y = random_cropXY(x, Y)
+    else:
+        x, Y = center_crop(x), center_crop(Y)
+    return x, mask_to_bb(Y)
+
+
+def create_corner_rect(bb, color='red'):
+    bb = np.array(bb, dtype=np.float32)
+    return plt.Rectangle((bb[1], bb[0]), bb[3] - bb[1], bb[2] - bb[0], color=color,
+                         fill=False, lw=3)
+
+
+def show_corner_bb(im, bb):
+    plt.imshow(im)
+    plt.gca().add_patch(create_corner_rect(bb))
+
+
+# original
+print(str(df_train.values[68][8]))
+im = cv2.imread(str(df_train.values[68][8]))
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+show_corner_bb(im, df_train.values[68][9])
